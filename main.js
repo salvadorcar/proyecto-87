02a64acc67424d1fc04a635cf70360758d132833
salvadorcar <143309978+salvadorcar@users.noreply.github.com>
2023-11-04 23:17:10 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ con un ancho de 430 y una altura de 200.
 ctx.beginPath_______;
 ctx.strokeStyle_______ = "grey";
 ctx.lineWidth_______ = 1;
-ctx.rect_______(x=150, y=143, width=430, height=200);
+ctx.rect_______(x="150", y="143", width="430", height="200");
 ctx.stroke();
 
 /*
@@ -23,33 +23,33 @@ ctx.stroke();
 con un radio de 40, un startAngle de 0 y un endAngle de 2 * Math.PI.
 */
 
-ctx.beginPath_______;
-ctx.strokeStyle_______ = "blue";
-ctx.lineWidth_______ = 5;
-ctx.arc_______(x=150, y=143, r=40, startAngle=0, endAngle=2*Math.PI);
+ctx.beginPath();
+ctx.strokeStyle= blue;
+ctx.lineWidth = 5;
+ctx.arc(150, 143, 40, 0, 2*Math.PI);
 ctx.stroke();
 
 // De igual forma, crea un círculo negro en la posición 350 y 210
-ctx.beginPath_______;
-ctx.strokeStyle_______ = "black";
-ctx.lineWidth_______ = 5;
-ctx.arc_______(x=350, y=210, startAngle=0, endAngle=2*Math.PI);
+ctx.beginPath();
+ctx.strokeStyle= black;
+ctx.lineWidth = 5;
+ctx.arc(350, 210, 40, 0, 2*Math.PI);
 ctx.stroke();
 // De igual forma, crea un círculo rojo en la posición 210 y 40
-ctx.beginPath_______;
-ctx.strokeStyle_______ = "red";
-ctx.lineWidth_______ = 5;
-ctx.arc_______(x=300, y=250, startAngle=0, endAngle=2*Math.PI);
+ctx.beginPath();
+ctx.strokeStyle= red;
+ctx.lineWidth = 5;
+ctx.arc(210, 40, 40, 0, 2*Math.PI);
 ctx.stroke();
 // De igual forma, crea un círculo naranja en la posición 300 y 250
-ctx.beginPath_______;
-ctx.strokeStyle_______ = "orange";
-ctx.lineWidth_______ = 5;
-ctx.arc_______(x=300, y=250, startAngle=0, endAngle=2*Math.PI);
+ctx.beginPath();
+ctx.strokeStyle= orange;
+ctx.lineWidth = 5;
+ctx.arc(300, 250, 40, 0, 2*Math.PI);
 ctx.stroke();
 // De igual forma, crea un círculo verde en la posición 400 y 250
-ctx.beginPath_______;
-ctx.strokeStyle_______ = "green";
-ctx.lineWidth_______ = 5;
-ctx.arc_______(x=400, y=250, startAngle=0, endAngle=2*Math.PI);
+ctx.beginPath();
+ctx.strokeStyle= red;
+ctx.lineWidth = 5;
+ctx.arc(210, 400, 250, 0, 2*Math.PI);
 ctx.stroke();
